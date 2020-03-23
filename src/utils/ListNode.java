39dd302a7db3674bef5aa3920d51208528728857
huangzuo <huangzuo@mi-ya.com.cn>
2020-03-23 23:24:51 +0800

    commit utils ListNode & TreeNode

---
+++ src/utils/ListNode.java
@@ -0,0 +1,15 @@
+/*
+ * Miya.com Inc.
+ * Copyright (c) 2004-2020 All Rights Reserved.
+ */
+package utils;
+
+/**
+ * @author huangzuo
+ * @version : ListNode.java, v 0.1 2020年03月23日 11:24 下午 huangzuo Exp $
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
---
+++ src/utils/TreeNode.java
@@ -0,0 +1,16 @@
+/*
+ * Miya.com Inc.
+ * Copyright (c) 2004-2020 All Rights Reserved.
+ */
+package utils;
+
+/**
+ * @author huangzuo
+ * @version : TreeNode.java, v 0.1 2020年03月23日 11:23 下午 huangzuo Exp $
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
