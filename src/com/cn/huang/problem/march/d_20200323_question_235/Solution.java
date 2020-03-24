2a3b8cd763c254a45803fcc527f463354e4f0d8f
huangzuo <huangzuo@mi-ya.com.cn>
2020-03-24 15:27:24 +0800

    push

---
+++ src/com/cn/huang/problem/march/d_20200323_question_235/Solution.java
@@ -0,0 +1,31 @@
+/*
+ * Miya.com Inc.
+ * Copyright (c) 2004-2020 All Rights Reserved.
+ */
+package com.cn.huang.problem.march.d_20200323_question_235;
+
+
+import com.cn.huang.utils.TreeNode;
+
+/**
+ * @author huangzuo
+ * @version : Solution.java, v 0.1 2020年03月24日 12:02 上午 huangzuo Exp $
+ */
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        //找出p和q的所有父节点
+
+
+        return null;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        String str = "[6,2,8,0,4,7,9,null,null,3,5]";
+        TreeNode node = TreeNode.mkTree(str);
+
+    }
+}
