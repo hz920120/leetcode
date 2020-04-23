95ad9ff8ebc86df42af66955820e5e62bb6ba305
huangzuo <huangzuo@mi-ya.com.cn>
2020-04-23 23:53:35 +0800

    committed primary algorithm successfully

---
+++ src/com/cn/huang/array/SingleNumber.java
@@ -0,0 +1,43 @@
+/*
+ * Miya.com Inc.
+ * Copyright (c) 2004-2020 All Rights Reserved.
+ */
+package com.cn.huang.array;
+
+import java.util.Arrays;
+
+/**
+ *
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ *
+ * 说明：
+ *
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ *
+ * 示例 1:
+ *
+ * 输入: [2,2,1]
+ * 输出: 1
+ * 示例 2:
+ *
+ * 输入: [4,1,2,1,2]
+ * 输出: 4
+ *
+ * @author huangzuo
+ * @version : SingleNumber.java, v 0.1 2020年04月23日 11:34 下午 huangzuo Exp $
+ */
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        if (nums.length == 0){return 0;}
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] == nums[i + 1]) {
+                i++;
+            }else {
+                return nums[i];
+            }
+        }
+        return nums[nums.length - 1];
+    }
+}
