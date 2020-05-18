873da0bc6759297db40c6f56b7fedd0d60eb1b1d
huangzuo <huangzuo@mi-ya.com.cn>
2020-05-18 10:06:04 +0800

    update TreeNode

--- src/com/cn/huang/utils/TreeNode.java
+++ src/com/cn/huang/utils/TreeNode.java
@@ -12,7 +12,13 @@ public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
-    public TreeNode(int x) { this.val = x; }
+    public TreeNode() {}
+    public TreeNode(int val) { this.val = val; }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 
     public String toString(){
         return Integer.toString(val);
