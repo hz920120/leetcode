f4c9ffbfd1f22ec7b00b3357d6dfb00df2a983ef
huangzuo <huangzuo@mi-ya.com.cn>
2020-05-22 10:34:59 +0800

    committed primary algorithm successfully

--- src/com/cn/huang/list/RemoveNthFromEnd.java
+++ src/com/cn/huang/list/RemoveNthFromEnd.java
@@ -28,35 +28,31 @@ import com.cn.huang.utils.ListNode;
  */
 public class RemoveNthFromEnd {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-
-        // 开辟两个指针，且第二个指针距离第一个指针为n+1，
-        // 因为要删除节点，必须获得该待删除节点的前一个节点。。。。。
-        ListNode node1 = head;
-        ListNode node2 = head;
-        node2 = move(node2,n+1);
-
-        // 判定一下特殊情况
-        // 比如链表长度为5，删除倒数第五个，就是删除第一个。
-        if(null == move(head,n)) return head.next;
-
-        // 移动两个指针，直到后面的指针移动到了null节点，也就是链表结束位置
-        while(node2 != null){
-            node1 = move(node1,1);
-            node2 = move(node2,1);
+        //没有节点
+        if (head == null){
+            return null;
         }
-
-        // 删除倒数第n个节点
-        node1.next = node1.next.next;
-        return head;
+        ListNode end,now = head,pre = head;
+        for (int i = 0; ; i++) {
+            end = getNode(now,n);
+            if (i==0 && end == null){
+                return head.next;
+            }
+            if (end == null){
+                pre.next = now.next;
+                return head;
+            }else {
+                pre = now;
+                now = now.next;
+            }
+        }
     }
 
-    // move towards the end of list
-    ListNode move(ListNode node, int step){
-        for(int i = 0; i<step; i++){
-            if(node.next == null) return null;
-            node = node.next;
+    private ListNode getNode(ListNode head,int n){
+        for (int i = 0; i < n; i++) {
+            head = head.next;
         }
-        return node;
+        return head;
     }
 
 
@@ -68,5 +64,6 @@ public class RemoveNthFromEnd {
         head.next.next.next = new ListNode(4);
         head.next.next.next.next = new ListNode(5);
         removeNthFromEnd.removeNthFromEnd(head,2);
+        System.out.println(1);
     }
 }
