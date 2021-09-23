1546a258951181b75ba9a61151bfc04a114019c2
huangzuo <huangzuo@mi-ya.com.cn>
2021-09-23 10:49:22 +0800

    commit

---
+++ src/com/cn/sep2021/IsPowerOfThree.java
@@ -0,0 +1,24 @@
+package com.cn.sep2021;
+
+/**
+ * @author HUANG Zuo
+ * @version create_time：2021-09-23 10:41:24
+ * @declaration
+ * @e-mail hz920120@gmail.com
+ */
+public class IsPowerOfThree {
+    public boolean isPowerOfThree(int n) {
+        return process(n);
+    }
+
+    public boolean process(double n) {
+        if (n == 1) return true;
+        if (n < 1) return false;
+        return process(n / 3L);
+    }
+
+    public static void main(String[] args) {
+        IsPowerOfThree isPowerOfThree = new IsPowerOfThree();
+        System.out.println(isPowerOfThree.isPowerOfThree(45));
+    }
+}
