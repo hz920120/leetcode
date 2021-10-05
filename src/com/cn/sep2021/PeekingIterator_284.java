47d75909312b92281a8e8d484c683dd7ef085cbb
huangzuo <huangzuo@mi-ya.com.cn>
2021-10-05 09:55:27 +0800

    commit 284

---
+++ src/com/cn/sep2021/PeekingIterator_284.java
@@ -0,0 +1,39 @@
+package com.cn.sep2021;
+
+import java.util.Iterator;
+
+/**
+ * @author HUANG Zuo
+ * @version create_time：2021-10-05 09:48:36
+ * @declaration
+ * @e-mail hz920120@gmail.com
+ */
+public class PeekingIterator_284 implements Iterator<Integer> {
+
+    Iterator<Integer> iter;
+
+    Integer next;
+
+    public PeekingIterator_284(Iterator<Integer> iterator) {
+        // initialize any member here.
+        iter = iterator;
+        next = iterator.next();
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    public Integer peek() {
+        return next;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return next != null;
+    }
+
+    @Override
+    public Integer next() {
+        Integer res = next;
+        next = iter.hasNext() ? iter.next() : null;
+        return res;
+    }
+}
